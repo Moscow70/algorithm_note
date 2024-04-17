bd2c954a90acc03d68f10762eeb5b27712251cb4
Moscow70 <gshuai734@gmail.com>
2024-04-17 17:22:07 +0800

    4.17 7th problem

---
+++ 4.3.7.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector <vector <int>> num;
+int layer;
+
+int recursion(int row, int col) {
+    if (row == layer - 1) {
+        return num[row][col];
+    }
+    else {
+        return max(recursion(row + 1, col), recursion(row + 1, col + 1)) + num[row][col];
+    }
+}
+
+int main () {
+
+    scanf("%d", &layer);
+    vector <int> thislayer;
+    for (int i = 0; i < layer; i++) {
+        thislayer.clear();
+        for (int j = 0; j <= i; j++) {
+            int temp;
+            cin >> temp;
+            thislayer.push_back(temp);
+        }
+        num.push_back(thislayer);
+    }
+    int res;
+    res = recursion(0, 0);
+    system("pause");
+    return 0;
+}
