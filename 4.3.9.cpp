ed99c725a45d3c3df8876a7b3a3498412551ddbe
Moscow70 <gshuai734@gmail.com>
2024-04-18 14:53:21 +0800

    4.18 1st problem

---
+++ 4.3.9.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int recursion(int n) {
+    if (n == 1) {
+        return 1;
+    }
+    else if (n == 2) {
+        return 1;
+    }
+    else {
+        return recursion(n - 1) + recursion(n - 2);
+    }
+}
+
+int main () {
+    int n;
+    scanf("%d", &n);
+    int res;
+    res = recursion(n);
+    printf("%d", n);
+    system("pause");
+    return 0;
+}
+
+/*
+这题没搞清关系式，到达第n层的方式就是可以先到它前一层或者前两层，两个相加即可
+*/
